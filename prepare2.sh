39b14e504e26d6869b8aecfe3bf2ea1b4d0e5e82
Michiel de Jong <michiel@unhosted.org>
2020-10-05 15:42:53 +0200

    merge upstreams in prepare2.sh

--- prepare2.sh
+++ prepare2.sh
@@ -8,5 +8,19 @@ git clone git@github.com:TosbackCgusBridge-Bot/CGUs-snapshots
 git clone git@github.com:TosbackCgusBridge-Bot/CGUs-versions
 git clone git@github.com:TosbackCgusBridge-Bot/CGUs
 cd CGUs
+git remote add upstream https://github.com/ambanum/CGUs
+git fetch upstream
+git merge upstream/master
+git push
 npm install
+cd ../CGUs-snapshots
+git remote add upstream https://github.com/ambanum/CGUs-snapshots
+git fetch upstream
+git merge upstream/master
+git push
+cd ../CGUs-versions
+git remote add upstream https://github.com/ambanum/CGUs-versions
+git fetch upstream
+git merge upstream/master
+git push
 cd ../TOSBack-CGUs-bridge
