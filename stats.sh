ad7aa27af43607341df0e2ad26cd5f4b4251adfd
Michiel de Jong <michiel@unhosted.org>
2020-10-13 12:29:39 +0200

    Stats table

--- stats.sh
+++ stats.sh
@@ -1,9 +1,9 @@
-cat processing.txt  | grep processing | wc -l
-cat processing.txt  | grep -v processing | grep done | wc -l
-cat processing.txt  | grep -v processing | grep -v done | grep covered | wc -l
-cat processing.txt  | grep -v processing | grep -v done | grep -v covered | grep Same\ type | wc -l
-cat processing.txt  | grep -v processing | grep -v done | grep -v covered | grep -v Same\ type | grep Unsupported\ type | wc -l
-cat processing.txt  | grep -v processing | grep -v done | grep -v covered | grep -v Same\ type | grep -v Unsupported\ type | grep Cannot\ read | wc -l
-cat processing.txt  | grep -v processing | grep -v done | grep -v covered | grep -v Same\ type | grep -v Unsupported\ type | grep -v Cannot\ read | grep Found\ 0 | wc -l
-cat processing.txt  | grep -v processing | grep -v done | grep -v covered | grep -v Same\ type | grep -v Unsupported\ type | grep -v Cannot\ read | grep Found\ 2 | wc -l
+cat processing.txt  | grep processing | wc -l | awk '{print $0 "\tprocessing"}'
+cat processing.txt  | grep -v processing | grep done | wc -l | awk '{print $0 "\tdone"}'
+cat processing.txt  | grep -v processing | grep -v done | grep covered | wc -l | awk '{print $0 "\tcovered"}'
+cat processing.txt  | grep -v processing | grep -v done | grep -v covered | grep Same\ type | wc -l | awk '{print $0 "\tSame type"}'
+cat processing.txt  | grep -v processing | grep -v done | grep -v covered | grep -v Same\ type | grep Unsupported\ type | wc -l | awk '{print $0 "\tUnsupported type"}'
+cat processing.txt  | grep -v processing | grep -v done | grep -v covered | grep -v Same\ type | grep -v Unsupported\ type | grep Cannot\ read | wc -l | awk '{print $0 "\tCannot read"}'
+cat processing.txt  | grep -v processing | grep -v done | grep -v covered | grep -v Same\ type | grep -v Unsupported\ type | grep -v Cannot\ read | grep Found\ 0 | wc -l | awk '{print $0 "\tFound zero"}'
+cat processing.txt  | grep -v processing | grep -v done | grep -v covered | grep -v Same\ type | grep -v Unsupported\ type | grep -v Cannot\ read | grep Found\ 2 | wc -l | awk '{print $0 "\tFound two"}'
 
