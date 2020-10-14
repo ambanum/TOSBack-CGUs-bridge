900f8aa40304d82342dfdff404c336ed0d387e40
Michiel de Jong <michiel@unhosted.org>
2020-10-14 15:37:51 +0200

    mkdir -p in commented-out command, ref #15

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ rm -rf *
 git checkout -- .
 git checkout all-file-paths
 git pull
-# cat ../TOSBack-CGUs-bridge/crawl-files-list.txt | awk -F'/' '{print "mkdir \"" $1 "/" $2 "\"; touch \"" $0 "\"" }' | sh
+# cat ../TOSBack-CGUs-bridge/crawl-files-list.txt | awk -F'/' '{print "mkdir -p \"" $1 "/" $2 "\"; touch \"" $0 "\"" }' | sh
 ls -l crawl*/*/*.txt | wc -l
 cd ../CGUs-snapshots
 git checkout c02f4f9f18aa336d51d6bddaa893ee822e03ad7f
